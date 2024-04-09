c52a8169c622f1e22a72f540b2ff310b9e37dd28
Haroon Khan <HaroonKhan120704@gmail.com>
2024-04-09 04:32:08 +0100

    get token decoded token and got username

--- JS/Home.js
+++ JS/Home.js
@@ -1,1 +1,25 @@
 'use strict';
+const username = document.getElementById('username');
+let GetCookieToken = function (name) {
+  let value = '; ' + document.cookie;
+  let parts = value.split('; ' + name + '=');
+  if (parts.length == 2) return parts.pop().split(';').shift();
+};
+
+let cookieVal = GetCookieToken('token');
+console.log(cookieVal);
+
+function decodeJWT(token) {
+  const parts = token.split('.');
+  const header = JSON.parse(atob(parts[0]));
+  const payload = JSON.parse(atob(parts[1]));
+
+  return {
+    header: header,
+    payload: payload,
+  };
+}
+const jwt = cookieVal;
+const decodedJWT = decodeJWT(jwt);
+const JWTusername = decodedJWT.payload.username;
+username.innerHTML = JWTusername;
