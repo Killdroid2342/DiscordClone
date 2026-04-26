2ef8d01d0d7b47db0f3745311e73619be90db952
Haroon Khan <HaroonKhan120704@gmail.com>
2026-04-26 15:08:39 +0100

    cleaned up paths

---
+++ JS/AppPaths.js
@@ -0,0 +1,23 @@
+(function () {
+  const isFileProtocol = window.location.protocol === 'file:';
+
+  function normalizePath(path) {
+    return String(path || '').replace(/^\/+/, '');
+  }
+
+  window.APP_PATHS = {
+    apiBase: 'http://localhost:5018',
+    isFileProtocol,
+    assetUrl(path) {
+      const normalizedPath = normalizePath(path);
+      return isFileProtocol ? `../${normalizedPath}` : `/${normalizedPath}`;
+    },
+    dependencyUrl(path) {
+      const normalizedPath = normalizePath(path);
+      return isFileProtocol ? `../${normalizedPath}` : `/${normalizedPath}`;
+    },
+    pageUrl(pageName) {
+      return isFileProtocol ? `./${pageName}` : `/Pages/${pageName}`;
+    },
+  };
+})();
