065d496fa68bbbcef965bdf8c12f3f7e7d8015e5
Haroon Khan <HaroonKhan120704@gmail.com>
2026-04-27 13:33:18 +0100

    feat: update package.json and webpack.config.js for improved build process
    - Updated project description in package.json.
    - Changed Electron version to ^41.3.0 and updated axios version to ^1.15.2.
    - Added electron-builder for packaging and distribution.
    - Defined build configuration for electron-builder in package.json.
    - Modified webpack.config.js to set development mode and updated output path to 'dist'.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: '/JS/Home.js',
+  mode: 'development',
+  entry: './JS/Home.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'src/public'),
-  },
-  module: {
-    rules: [
-      {
-        test: /.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
+    path: path.resolve(__dirname, 'dist'),
   },
 };
