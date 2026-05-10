aa41931792e9c49fe6cd2fa382df2ef4c427eadc
Haroon Khan <HaroonKhan120704@gmail.com>
2026-05-10 19:08:33 +0100

    feat: add comment to clarify API port configuration in AppConfig.js

--- JS/AppConfig.js
+++ JS/AppConfig.js
@@ -1,5 +1,6 @@
 (function () {
   window.MYDISCORD_CONFIG = {
+    // 5018 is the ASP.NET API port. Serve the browser frontend on a different port, such as 8080.
     apiBase: 'http://localhost:5018',
     cdnBase: '',
   };
